e3d74bf4a5f23d5c9d640dd26f704fe4236e3de9
Ravi <ayswarym04@gmail.com>
2024-09-09 07:36:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ var countryStateCityInfo = {
 const countrySelect = document.getElementById("c");
 const stateSelect = document.getElementById("s2");
 const citySelect = document.getElementById("s3");
-const names=document.getElementById("name")
-const age=document.getElementById("age");
-const phnum=document.getElementById("phnum");
-const submitbtn=document.getElementById("submitbtn");
+const names = document.getElementById("name")
+const age = document.getElementById("age");
+const phnum = document.getElementById("phnum");
+const submitbtn = document.getElementById("submitbtn");
 
 function selectCountry() {
     stateSelect.disabled = true;
@@ -53,13 +53,13 @@ function selectCountry() {
     }
 }
 
-countrySelect.addEventListener('change', function(e) {
+countrySelect.addEventListener('change', function (e) {
     stateSelect.disabled = false;
-    stateSelect.innerHTML="<option>Select The State</option>"
+    stateSelect.innerHTML = "<option>Select The State</option>"
     const selectedCountry = e.target.value;
-    if (selectedCountry){
-        state=countryStateCityInfo[selectedCountry]
-        for (let states in state){
+    if (selectedCountry) {
+        state = countryStateCityInfo[selectedCountry]
+        for (let states in state) {
             const option = document.createElement("option");
             option.value = states
             option.text = states
@@ -68,26 +68,28 @@ countrySelect.addEventListener('change', function(e) {
     }
 })
 
-stateSelect.addEventListener('change', function(e) {
+stateSelect.addEventListener('change', function (e) {
     stateSelect.disabled = false;
     citySelect.disabled = false;
-    citySelect.innerHTML="<option>Select The City</option>"
+    citySelect.innerHTML = "<option>Select The City</option>"
     const country = countrySelect.value;
-    const state= e.target.value
-    cities=countryStateCityInfo[country][state]; 
-    for(let city in cities){
-        const option = document.createElement("option");
+    const state = e.target.value
+    if (country && state) {
+        cities = countryStateCityInfo[country][state];
+        for (let city in cities) {
+            const option = document.createElement("option");
             option.value = city
             option.text = city
             citySelect.appendChild(option)
+        }
     }
 })
 
-submitbtn.addEventListener('click',(e)=>{
-    if(names.value=="" || age.value=="" || phnum.value=="" || countrySelect.value=="" || stateSelect.value=="" || citySelect.value=="" ){
+submitbtn.addEventListener('click', (e) => {
+    if (names.value == "" || age.value == "" || phnum.value == "" || countrySelect.value == "" || stateSelect.value == "" || citySelect.value == "") {
         e.preventDefault
         alert("Enter All Values....")
-    }else{
+    } else {
         alert(`You Entered Values Are
         Name: ${names.value}
         Age:${age.value}
@@ -95,12 +97,13 @@ submitbtn.addEventListener('click',(e)=>{
         Country:${countrySelect.value}
         State:${stateSelect.value}
         City:${citySelect.value}`)
+        names.value = ""
+        age.value = ""
+        phnum.value = ""
+        countrySelect.value = ""
+        stateSelect.disabled = true
+        citySelect.disabled = true
     }
-    names.value=""
-    age.value=""
-    phnum.value=""
-    countrySelect.value=""
-    stateSelect.disabled=true
-    citySelect.disabled=true
+
 })
 selectCountry()
